964fde477031f0a26a5f696b9e7ddc0e893cea39
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-31 16:35:31 +0200

    i added the comments. ho aggiunto i commenti.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+// Chiede all'utente di inserire una parola
 let parola = prompt("Inserisci una parola");
 
+// Funzione che controlla se la parola è palindroma
 function palindroma(parola) {
+    
+    // Inverte la parola
     let parolaRiverso = parola.split("").reverse().join("");
+    
+    // Controlla se la parola invertita è uguale a quella originale
     if (parolaRiverso === parola) {
-        return true;
-    } else { return false; }
+        return true; // La parola è palindroma
+    } else {
+        return false; // La parola non è palindroma
+    }
 }
-if(palindroma(parola)){
-    console.log('parola è palindra');
-} else{
-    console.log('Palora Non è Palindra');
 
+// Verifica il risultato della funzione e stampa il messaggio
+if (palindroma(parola)) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
 }
